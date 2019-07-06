a2d5ab8f342ae4b190a1cd0bbd779d225ce11037
ATranimal <tran.andrew.a@gmail.com>
2019-07-06 17:24:03 -0400

    Boilerplate for player.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,20 @@
 var imgs_footholds = [];
+var imgs_earl_idle = [];
 var footpath;
 
 function preload() {
   for (var numberOfRock = 1; numberOfRock <= 4; numberOfRock++) {
     imgs_footholds.push(loadImage("images/rocks/rock" + numberOfRock + ".png"));
   }
+
+  imgs_earl_idle.push(loadImage("images/player/idle1.png"));
+  imgs_earl_idle.push(loadImage("images/player/idle2.png"));
 }
 
 function setup() {
   createCanvas(800, windowHeight);
-  footpath = new Footpath();
+  footpath = new Footpath(imgs_footholds);
+  early = new Player();
 }
 
 function draw() {
